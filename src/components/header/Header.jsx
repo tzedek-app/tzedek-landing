775daa11d631dd5901dc5435b0b5c70c5fbcc1cf
TabarakoAkula <mishabor2367@gmail.com>
2025-05-13 23:32:17 +0300

    👩‍🏫Add Header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Header.css"
+
+export default function Header() {
+    return (
+        <header>
+            <p>TZEDEK</p>
+            <p>ABOUT</p>
+            <p>TRY IT</p>
+        </header>
+    );
+}
